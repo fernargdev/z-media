b678ab4b94ea07c79332069592ed4a62981b4d13
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-09-14 11:48:57 -0400

    add-question

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import {
+  addQuestion,
   editCourse,
   getAllCourses,
   getCourseByUser,
@@ -31,5 +32,7 @@ courseRouter.get('/get-course/:id', getSingleCourse);
 courseRouter.get('/get-courses', getAllCourses);
 
 courseRouter.get('/get-course-content/:id', isAuthenticated, getCourseByUser);
+
+courseRouter.get('/add-question', isAuthenticated, addQuestion);
 
 export default courseRouter;
