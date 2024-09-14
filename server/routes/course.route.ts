5e193d622424c0349d902b7e9ca965123959f0b4
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-09-13 23:12:52 -0400

    get course content --- only for valid user

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import {
   editCourse,
   getAllCourses,
+  getCourseByUser,
   getSingleCourse,
   uploadCourse,
 } from '../controllers/course.controller';
@@ -28,5 +29,7 @@ courseRouter.put(
 courseRouter.get('/get-course/:id', getSingleCourse);
 
 courseRouter.get('/get-courses', getAllCourses);
+
+courseRouter.get('/get-course-content/:id', isAuthenticated, getCourseByUser);
 
 export default courseRouter;
