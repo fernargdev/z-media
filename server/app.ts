7097d105c4f227f5c77cd30c103c03541fc8852c
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-09-17 14:39:45 -0400

    create-order

--- server/app.ts
+++ server/app.ts
@@ -10,6 +10,7 @@ import { ErrorMidleware } from './middleware/error';
 
 import userRouter from './routes/user.route';
 import courseRouter from './routes/course.route';
+import orderRouter from './routes/order.route';
 
 // body parser
 app.use(express.json({ limit: '50mb' }));
@@ -33,8 +34,7 @@ app.get('/api/v1/test', (req: Request, res: Response, next: NextFunction) => {
 });
 
 // routers
-app.use('/api/v1', userRouter);
-app.use('/api/v1', courseRouter);
+app.use('/api/v1', userRouter, courseRouter, orderRouter);
 
 // unknown route
 app.all('*', (req: Request, res: Response, next: NextFunction) => {
