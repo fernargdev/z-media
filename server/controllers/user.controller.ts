a3d344cbccee36c5cadb947234954e60290e6f03
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-09-18 22:03:01 -0400

    update-user-role

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -17,7 +17,11 @@ import {
   sendToken,
 } from '../utils/jwt';
 import { redis } from '../utils/redis';
-import { getUserById, getAllUsersService } from '../services/user.service';
+import {
+  getUserById,
+  getAllUsersService,
+  updateUserRoleService,
+} from '../services/user.service';
 import cloudinary from 'cloudinary';
 
 //  register user
@@ -451,6 +455,19 @@ export const getAllUsers = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       getAllUsersService(res);
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
+
+// update user role --- only for admin
+export const updateUserRole = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id, role } = req.body;
+
+      updateUserRoleService(res, id, role);
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 400));
     }
--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -12,6 +12,7 @@ import {
   updatePassword,
   updateProfilePicture,
   updateUserInfo,
+  updateUserRole,
 } from '../controllers/user.controller';
 
 import { authorizeRoles, isAuthenticated } from '../middleware/auth';
@@ -43,6 +44,13 @@ userRouter.get(
   isAuthenticated,
   authorizeRoles('admin'),
   getAllUsers
+);
+
+userRouter.put(
+  '/update-user-role',
+  isAuthenticated,
+  authorizeRoles('admin'),
+  updateUserRole
 );
 
 export default userRouter;
