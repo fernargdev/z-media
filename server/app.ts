e09ca1e242014185271630361b918bb928915e28
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-09-26 15:27:32 -0400

    Get the last 28 days users,orders, courses for the last 1year

--- server/app.ts
+++ server/app.ts
@@ -12,6 +12,7 @@ import userRouter from './routes/user.route';
 import courseRouter from './routes/course.route';
 import orderRouter from './routes/order.route';
 import notificationRouter from './routes/notification.route';
+import analyticsRouter from './routes/analytics.route';
 
 // body parser
 app.use(express.json({ limit: '50mb' }));
@@ -35,7 +36,14 @@ app.get('/api/v1/test', (req: Request, res: Response, next: NextFunction) => {
 });
 
 // routers
-app.use('/api/v1', userRouter, courseRouter, orderRouter, notificationRouter);
+app.use(
+  '/api/v1',
+  userRouter,
+  courseRouter,
+  orderRouter,
+  notificationRouter,
+  analyticsRouter
+);
 
 // unknown route
 app.all('*', (req: Request, res: Response, next: NextFunction) => {
